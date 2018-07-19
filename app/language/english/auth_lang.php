5aada4bdfb560db6e4928230539e2ce8e785fa70
Handesk Kim <handeskdotvn@gmail.com>
2018-07-19 02:38:39 -0700

    add source

---
+++ app/language/english/auth_lang.php
@@ -0,0 +1,94 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * Module: Auth
+ * Language: English
+ * 
+ * Last edited:
+ * 30th April 2015
+ *
+ * Package:
+ * Simple POS - Point of sale made easy
+ * 
+ * You can translate this file to your language. 
+ * For instruction on new language setup, please visit the documentations. 
+ * You also can share your language files by emailing to saleem@tecdiary.com 
+ * Thank you 
+ */
+
+$lang['sign_in']                                    = "Đăng nhập";
+$lang['notify_user_by_email']                       = "Cảnh báo người dùng bằng emai";
+$lang['group']                                      = "Nhóm";
+$lang['edit_user']                                  = "Sửa người dùng";
+$lang['delete_user']                                = "Xóa người dùng";
+$lang['user_added']                                 = "Thêm người dùng thành công";
+$lang['user_updated']                               = "Cập nhật người dùng thành công";
+$lang['users_deleted']                              = "Xóa người dùng thành công";
+$lang['alert_x_user']                               = "You are going to remove this user permanently. Press OK to proceed and Cancel to Go Back";
+$lang['login_email']                                = "Đăng nhập email";
+$lang['edit_profile']                               = "Sửa thông tin";
+$lang['website']                                    = "Website";
+$lang['if_you_need_to_rest_password_for_user']      = "If you need to reset the password for this user.";
+$lang['user_options']                               = "Tùy chọn";
+$lang['old_password']                               = "Mật khẩu cũ";
+$lang['new_password']                               = "Mật khẩu mới";
+$lang['change_avatar']                              = "Đổi ảnh";
+$lang['update_avatar']                              = "Cập nhật ảnh";
+$lang['avatar']                                     = "Avatar";
+$lang['avatar_deleted']                             = "Cập nhật ảnh thành công";
+$lang['captcha_wrong']                              = "Captcha is wrong or expired. Please try again";
+$lang['captcha']                                    = "Captcha";
+$lang['site_is_offline_plz_try_later']              = "Site is offline. Please visit us again in few days.";
+$lang['type_captcha']                               = "Loại capcha";
+$lang['we_are_sorry_as_this_sction_is_still_under_development'] = "We are sorry as this section is still under development and we are try to make it asap. Your patient is highly appreciated.";
+$lang['confirm']                                    = "Confirmation";
+$lang['error_csrf']                                 = "Cross-site request forgery detected or the csrf token is expired. Please try again.";
+$lang['avatar_updated']                             = "Avatar successfully updated";
+$lang['registration_is_disabled']                   = "Đăng ký không mở.";
+$lang['login_to_your_account']                      = "Vui lòng đăng nhập tài khoản.";
+$lang['pw']                                         = "Mật khẩu";
+$lang['remember_me']                                = "Nhớ tôi";
+$lang['forgot_your_password']                       = "Quên mật khẩu?";
+$lang['dont_worry']                                 = "Đừng lo lắng!";
+$lang['click_here']                                 = "vào đây";
+$lang['to_rest']                                    = "để reset";
+$lang['forgot_password']                            = "Quên mật khẩu?";
+$lang['login_successful']                           = "Bạn đã đăng nhập thành công.";
+$lang['back']                                       = "Quay lại";
+$lang['dont_have_account']                          = "Không có tài khoản?";
+$lang['no_worry']                                   = "Đừng lo lắng!";
+$lang['to_register']                                = "để đăng ký";
+$lang['register_account_heading']                   = "Điền đầy đủ thông tin phía dưới để đăng kí";
+$lang['register_now']                               = "Đăng ký luôn";
+$lang['no_user_selected']                           = "No user selected. Please select at least one user.";
+$lang['delete_users']                               = "Xóa người dùng";
+$lang['delete_avatar']                              = "Xóa avatar";
+$lang['deactivate_heading']                         = "Are you sure to deactivate the user?";
+$lang['deactivate']                                 = "Deactivate";
+$lang['pasword_hint']                               = "At least 1 capital, 1 lowercase, 1 number and more than 8 characters long";
+$lang['pw_not_same']                                = "The password and confirm password are not the same";
+$lang['reset_password']                             = "Reset Password";
+$lang['reset_password_link_alt']                    = "You can paste this code below in your url if the above link not working";
+$lang['email_forgotten_password_subject']           = "Reset Password Details";
+$lang['reset_password_email']                       = "Reset Password %s";
+$lang['back_to_login']                              = "Back to login";
+$lang['forgot_password_unsuccessful']               = "Reset password failed";
+$lang['forgot_password_successful']                 = "Password successfully reset, please use new password to login";
+$lang['password_change_unsuccessful']               = "Password change failed";
+$lang['password_change_successful']                 = "Password successfully changed";
+$lang['forgot_password_email_not_found']            = "The email address enter does not belong to any account.";
+$lang['login_unsuccessful']                         = "Login Failed, Please try again";
+$lang['email_forgot_password_link']                 = "Reset Password Link";
+$lang['reset_password_heading']                     = "Reset Password";
+$lang['reset_password_new_password_label']          = "New Password";
+$lang['reset_password_new_password_confirm_label']  = "Confirm New Password";
+$lang['register']                                   = "Register";
+$lang['reset_password_submit_btn']                  = "Reset Password";
+$lang['error_csrf']                                 = 'This form post did not pass our security checks.';
+$lang['account_creation_successful']                = "Account successfully created";
+$lang['old_password_wrong']                         = "Please type correct old password";
+$lang['sending_email_failed']                       = "Unable to send email, If you are admin then please check your settings otherwise contact admin.";
+$lang['deactivate_successful']                      = "User successfully deactivated";
+$lang['activate_successful']                        = "User successfully activated";
+$lang['login_timeout']                              = "You have 3 failed login attempts. Please try after 10 minutes";
+$lang['forgot_password_heading']                    = "Enter your e-mail address below to reset your password.";
